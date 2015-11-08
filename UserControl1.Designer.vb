f7943c252c674685c9c215471bcffb2173275af5
Ramon Simona <rsimona@gmail.com>
2015-11-08 14:06:54 +0100

    Commit inicial

---
+++ UserControl1.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UserControl1
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl1 reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Percent0 = New System.Windows.Forms.Label()
+        Me.Percent10 = New System.Windows.Forms.Label()
+        Me.Percent20 = New System.Windows.Forms.Label()
+        Me.Percent30 = New System.Windows.Forms.Label()
+        Me.Percent40 = New System.Windows.Forms.Label()
+        Me.Percent50 = New System.Windows.Forms.Label()
+        Me.Percent60 = New System.Windows.Forms.Label()
+        Me.Percent70 = New System.Windows.Forms.Label()
+        Me.Percent80 = New System.Windows.Forms.Label()
+        Me.Percent90 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Percent0
+        '
+        Me.Percent0.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Percent0.Location = New System.Drawing.Point(13, 10)
+        Me.Percent0.Name = "Percent0"
+        Me.Percent0.Size = New System.Drawing.Size(28, 12)
+        Me.Percent0.TabIndex = 0
+        '
+        'Percent10
+        '
+        Me.Percent10.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Percent10.Location = New System.Drawing.Point(47, 10)
+        Me.Percent10.Name = "Percent10"
+        Me.Percent10.Size = New System.Drawing.Size(28, 12)
+        Me.Percent10.TabIndex = 1
+        '
+        'Percent20
+        '
+        Me.Percent20.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Percent20.Location = New System.Drawing.Point(81, 10)
+        Me.Percent20.Name = "Percent20"
+        Me.Percent20.Size = New System.Drawing.Size(28, 12)
+        Me.Percent20.TabIndex = 2
+        '
+        'Percent30
+        '
+        Me.Percent30.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Percent30.Location = New System.Drawing.Point(115, 10)
+        Me.Percent30.Name = "Percent30"
+        Me.Percent30.Size = New System.Drawing.Size(28, 12)
+        Me.Percent30.TabIndex = 3
+        '
+        'Percent40
+        '
+        Me.Percent40.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Percent40.Location = New System.Drawing.Point(149, 10)
+        Me.Percent40.Name = "Percent40"
+        Me.Percent40.Size = New System.Drawing.Size(28, 12)
+        Me.Percent40.TabIndex = 4
+        '
+        'Percent50
+        '
+        Me.Percent50.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Percent50.Location = New System.Drawing.Point(183, 10)
+        Me.Percent50.Name = "Percent50"
+        Me.Percent50.Size = New System.Drawing.Size(28, 12)
+        Me.Percent50.TabIndex = 5
+        '
+        'Percent60
+        '
+        Me.Percent60.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Percent60.Location = New System.Drawing.Point(217, 10)
+        Me.Percent60.Name = "Percent60"
+        Me.Percent60.Size = New System.Drawing.Size(28, 12)
+        Me.Percent60.TabIndex = 6
+        '
+        'Percent70
+        '
+        Me.Percent70.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Percent70.Location = New System.Drawing.Point(251, 10)
+        Me.Percent70.Name = "Percent70"
+        Me.Percent70.Size = New System.Drawing.Size(28, 12)
+        Me.Percent70.TabIndex = 7
+        '
+        'Percent80
+        '
+        Me.Percent80.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Percent80.Location = New System.Drawing.Point(285, 10)
+        Me.Percent80.Name = "Percent80"
+        Me.Percent80.Size = New System.Drawing.Size(28, 12)
+        Me.Percent80.TabIndex = 8
+        '
+        'Percent90
+        '
+        Me.Percent90.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Percent90.Location = New System.Drawing.Point(319, 10)
+        Me.Percent90.Name = "Percent90"
+        Me.Percent90.Size = New System.Drawing.Size(28, 12)
+        Me.Percent90.TabIndex = 9
+        '
+        'UserControl1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Percent90)
+        Me.Controls.Add(Me.Percent80)
+        Me.Controls.Add(Me.Percent70)
+        Me.Controls.Add(Me.Percent60)
+        Me.Controls.Add(Me.Percent50)
+        Me.Controls.Add(Me.Percent40)
+        Me.Controls.Add(Me.Percent30)
+        Me.Controls.Add(Me.Percent20)
+        Me.Controls.Add(Me.Percent10)
+        Me.Controls.Add(Me.Percent0)
+        Me.Name = "UserControl1"
+        Me.Size = New System.Drawing.Size(360, 32)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Percent0 As Label
+    Friend WithEvents Percent10 As Label
+    Friend WithEvents Percent20 As Label
+    Friend WithEvents Percent30 As Label
+    Friend WithEvents Percent40 As Label
+    Friend WithEvents Percent50 As Label
+    Friend WithEvents Percent60 As Label
+    Friend WithEvents Percent70 As Label
+    Friend WithEvents Percent80 As Label
+    Friend WithEvents Percent90 As Label
+End Class
---
+++ UserControl1.vb
@@ -0,0 +1,34 @@
+﻿Public Class UserControl1
+    Private _max As Double = 100
+    Private _min As Double = 0
+    Private _valor As Double = 0
+
+    Public Property Max() As Double
+        Get
+            Return _max
+        End Get
+        Set(value As Double)
+            _max = value
+        End Set
+    End Property
+
+    Public Property Min() As Double
+        Get
+            Return _min
+        End Get
+        Set(value As Double)
+            _min = value
+        End Set
+    End Property
+
+    Public Property Valor() As Double
+        Get
+            Return _valor
+        End Get
+        Set(value As Double)
+            _valor = value
+        End Set
+    End Property
+    'fgsdfgsdfgsdfg
+    'asfasdfasdfasdf
+End Class
